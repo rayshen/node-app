4df682846c759313b87da06df90ea53ddf8462cb
Shen Zhiwei <rayshen1012@gmail.com>
2017-07-09 13:51:49 +0800

    update to react-view

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index',{
+      title: 'Express',
+      time:new Date().toString()
+  });
 });
 
 module.exports = router;
---
+++ views/index.jsx
@@ -0,0 +1,29 @@
+/**
+ * 此类方式只可用于静态页面渲染
+ */
+var React = require('react');
+var Layout = require('./layout');
+
+class Index extends React.Component {
+    constructor(props, context) {
+        super(props, context);
+    }
+
+    render() {
+        return (
+            <Layout title={this.props.title}>
+                <h1>{this.props.title}</h1>
+                <p>Welcome to {this.props.title}</p>
+                <p>
+                    time:{this.props.time}
+                </p>
+            </Layout>
+        );
+    }
+}
+
+Index.propTypes = {
+  title: React.PropTypes.string
+};
+
+module.exports = Index;
